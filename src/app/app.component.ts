78dc5ffccea4f671147c67331e7bea7b6e1f6f41
annamelnyk <anna_melnyk@epam.com>
2023-05-10 23:12:00 +1000

    Add header and toolbar components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '100-angular-challenges';
+  public toolbarLinks: string[] = [
+    'Components',
+    'Directives',
+    'Services',
+    'Pipes',
+    'Other',
+  ];
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
