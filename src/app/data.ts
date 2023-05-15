8c7df87991f37dda815e618529784f116ee62394
annamelnyk <anna_melnyk@epam.com>
2023-05-16 00:05:48 +1000

    Add fixes

--- src/app/data.ts
+++ src/app/data.ts
@@ -4,7 +4,7 @@ export const accordionData: IAccordionItem[] = [
   {
     title: 'Example 1',
     body: 'Lorem iCommodo qui nisi do dolore do ad et eu labore est eu nostrud ipsum. Cillum nisi culpa duis sunt exercitation pariatur tempor occaecat dolor dolor. Eiusmod aliquip veniam dolor non proident laborum elit sunt. Adipisicing laboris anim fugiat in ut ad minim est dolore aliqua minim nulla non. Velit sit amet ea tempor exercitation excepteur deserunt sit ut nulla. Dolor duis aliquip qui adipisicing fugiat ex nostrud Lorem commodo amet nulla deserunt labore ad. Tempor pariatur nostrud Lorem excepteur ex magna eiusmod dolore.',
-    isExpanded: false,
+    isExpanded: true,
   },
   {
     title: 'Example 2',
