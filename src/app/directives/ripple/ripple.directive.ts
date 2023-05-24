11564f5584f04d57f4f65948a93f10706402bdeb
annamelnyk <anna_melnyk@epam.com>
2023-05-24 22:27:38 +1000

    Add changes to Ripple directive

--- src/app/directives/ripple/ripple.directive.ts
+++ src/app/directives/ripple/ripple.directive.ts
@@ -1,17 +1,21 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2, ViewChild } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appRipple]'
+  selector: '[appRipple]',
 })
 export class RippleDirective {
   @Input() public appRippleClass: string = 'ripple';
 
-  constructor(private element: ElementRef, private renderer: Renderer2) {
-
-  }
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('mouseenter')
-  public onMouseEnterHandler(event: MouseEvent): void {
-    this.renderer.addClass(this.element.nativeElement, this.appRippleClass)
+  public onMouseEnterHandler(): void {
+    this.renderer.addClass(this.element.nativeElement, this.appRippleClass);
   }
 }
